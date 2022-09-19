5bd5361cda086f9b020a8cc53d337c9984efbf43
Basith Kunimal <basith374@gmail.com>
2022-09-19 20:32:55 +0400

    2d dotted background

--- components/ProductDetail/Images/index.js
+++ components/ProductDetail/Images/index.js
@@ -1,6 +1,36 @@
 import { assetHost } from "lib/constants";
 import Image from "next/image";
+import { useEffect, useRef } from "react";
 import styles from "./styles.module.scss";
+
+function Canvas({ children }) {
+  const canvas = useRef();
+  useEffect(() => {
+    const ctx = canvas.current.getContext("2d");
+    const width = canvas.current.offsetWidth;
+    const height = canvas.current.offsetHeight;
+    const s = 9;
+    const pL = 0;
+    const pT = 0;
+    const pR = s;
+    const pB = s;
+    const dot = 1;
+
+    ctx.fillStyle = "#cccccc";
+    for (var x = pL; x <= width - pR; x += s) {
+      for (var y = pT; y <= height - pB; y += s) {
+        ctx.fillRect(x, y, dot, dot);
+      }
+    }
+    ctx.stroke();
+  }, []);
+  return (
+    <div className={styles.canvas}>
+      <canvas ref={canvas} />
+      <div className={styles.preview}>{children}</div>
+    </div>
+  );
+}
 
 export default function Images({ product }) {
   const blueprint = product.blueprint.data.attributes;
@@ -18,12 +48,14 @@ export default function Images({ product }) {
       </div>
       <div className={styles.right}>
         <div className={styles.top}>
-          <Image
-            src={`${assetHost}${blueprint.url}`}
-            alt={blueprint.alternativeText}
-            layout="fill"
-            objectFit="contain"
-          />
+          <Canvas>
+            <Image
+              src={`${assetHost}${blueprint.url}`}
+              alt={blueprint.alternativeText}
+              layout="fill"
+              objectFit="contain"
+            />
+          </Canvas>
         </div>
         <div className={styles.bottom}>
           <Image
