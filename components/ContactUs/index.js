3337b40bdd44f823f4591158f41620d24b96afbf
Basith Kunimal <basith374@gmail.com>
2022-10-03 23:00:53 +0400

    dummy contact us form

--- components/ContactUs/index.js
+++ components/ContactUs/index.js
@@ -4,6 +4,8 @@ import icons from "./icons";
 import Head from "next/head";
 import Button from "components/shared/Button";
 import { siteName } from "lib/constants";
+import { Box, CircularProgress, Modal, Typography } from "@mui/material";
+import { useState } from "react";
 
 const contactInfo = [
   {
@@ -24,6 +26,19 @@ const contactInfo = [
 ];
 
 const ContactUs = () => {
+  const [submitted, setSubmitted] = useState(false);
+  const [busy, setBusy] = useState(false);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const data = {};
+    formData.forEach((value, key) => (data[key] = value));
+    setBusy(true);
+    setTimeout(() => {
+      setBusy(false);
+      setSubmitted(true);
+    }, 2000);
+  };
   return (
     <Layout>
       <Head>
@@ -34,26 +49,40 @@ const ContactUs = () => {
           <h1>Feel free to reach us</h1>
           <h2>For more info on our products</h2>
         </div>
-        <div className={`${styles.row} ${styles.colGap}`}>
+        <form className={`${styles.row} ${styles.colGap}`} onSubmit={onSubmit}>
           <div className={styles.contactForm}>
             <div className={`${styles.row} ${styles.fieldGap}`}>
               <div className={styles.field}>
                 <label>Name</label>
-                <input type="text" />
+                <input type="text" name="name" required />
               </div>
               <div className={styles.field}>
                 <label>Email</label>
-                <input type="text" />
+                <input type="email" name="email" required />
               </div>
             </div>
             <div>
               <div className={styles.field}>
                 <label>Message</label>
-                <textarea rows={5}></textarea>
+                <textarea rows={5} name="message" required></textarea>
               </div>
             </div>
             <div>
-              <Button>Send</Button>
+              <Button>
+                {busy ? (
+                  <div
+                    style={{
+                      display: "flex",
+                      height: "50px",
+                      alignItems: "center",
+                    }}
+                  >
+                    <CircularProgress sx={{ color: "white" }} size={25} />
+                  </div>
+                ) : (
+                  "Send"
+                )}
+              </Button>
             </div>
           </div>
           <div className={styles.contactInfo}>
@@ -72,7 +101,26 @@ const ContactUs = () => {
               </div>
             ))}
           </div>
-        </div>
+        </form>
+        <Modal open={submitted} onClose={() => setSubmitted(false)}>
+          <Box
+            sx={{
+              p: 3,
+              bgcolor: "#fff",
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              borderRadius: 2,
+              boxShadow: 24,
+            }}
+          >
+            <Typography variant="h6" sx={{ mb: 2 }}>
+              We thank you for your interest in Deess.
+            </Typography>
+            <Typography>You will receive get back to you soon.</Typography>
+          </Box>
+        </Modal>
       </div>
     </Layout>
   );
