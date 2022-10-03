83d09e602cdaa93c8961ea76d390863279e24c55
Basith Kunimal <basith374@gmail.com>
2022-10-03 21:07:08 +0400

    fake download form

--- components/Downloads/index.js
+++ components/Downloads/index.js
@@ -1,9 +1,12 @@
+import { CircularProgress, Modal, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import Button from "components/shared/Button";
 import Input from "components/shared/Input";
 import Layout from "components/shared/Layout";
 import Select from "components/shared/Select";
 import { siteName } from "lib/constants";
 import Head from "next/head";
+import { useState } from "react";
 import styles from "./styles.module.scss";
 
 const options = [
@@ -15,6 +18,19 @@ const options = [
 ];
 
 export default function Downloads() {
+  const [submitted, setSubmitted] = useState(false);
+  const [busy, setBusy] = useState(false);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const data = {};
+    formData.forEach((value, key) => (data[key] = value));
+    setBusy(true);
+    setTimeout(() => {
+      setBusy(false);
+      setSubmitted(true);
+    }, 2000);
+  };
   return (
     <Layout>
       <Head>
@@ -25,31 +41,66 @@ export default function Downloads() {
           <div className={styles.head}>
             <h1>Download your catalogue</h1>
           </div>
-          <div className={styles.form}>
+          <form className={styles.form} onSubmit={onSubmit}>
             <div className={styles.row}>
               <div className={styles.field}>
-                <Input label="Name" />
+                <Input label="Name" name="name" type="text" required />
               </div>
               <div className={styles.field}>
-                <Input label="Email" />
+                <Input label="Email" name="email" type="email" required />
               </div>
             </div>
             <div className={styles.row}>
               <div className={styles.field}>
-                <Input label="Company" />
+                <Input label="Company" type="text" />
               </div>
               <Select
                 options={options.map((option) => ({
                   label: option,
                   value: option,
                 }))}
+                name="type"
               />
             </div>
             <div>
-              <Button>Download</Button>
+              <Button>
+                {busy ? (
+                  <div
+                    style={{
+                      display: "flex",
+                      height: "50px",
+                      alignItems: "center",
+                    }}
+                  >
+                    <CircularProgress sx={{ color: "white" }} size={25} />
+                  </div>
+                ) : (
+                  "Download"
+                )}
+              </Button>
             </div>
-          </div>
+          </form>
         </div>
+        <Modal open={submitted} onClose={() => setSubmitted(false)}>
+          <Box
+            sx={{
+              p: 3,
+              bgcolor: "#fff",
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              borderRadius: 2,
+              boxShadow: 10,
+            }}
+          >
+            <h3>We thank you for your interest in Deess.</h3>
+            <p>
+              You will receive an email soon, with a link to download our
+              latetst catalogue
+            </p>
+          </Box>
+        </Modal>
       </div>
     </Layout>
   );
