e9b2aeedbae045449e7072b603c4c4084fa06b1e
Basith Kunimal <basith374@gmail.com>
2022-10-20 13:17:46 +0530

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import "../styles/globals.css";
 import NProgress from "nprogress";
 import "lib/nprogress.css";
 import { useEffect } from "react";
+import Script from "next/script";
 
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
@@ -11,7 +12,23 @@ function MyApp({ Component, pageProps }) {
   useEffect(() => {
     NProgress.configure({ showSpinner: false });
   }, []);
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+      >{`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.GA_ID}');`}</Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
