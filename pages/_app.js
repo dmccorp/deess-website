505e4dbc2517bf50e15b269a84412be3092599f2
Basith Kunimal <basith374@gmail.com>
2022-11-07 12:04:09 +0530

    correct env

--- pages/_app.js
+++ pages/_app.js
@@ -15,7 +15,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.REACT_APP_GA_ID}`}
         strategy="afterInteractive"
       />
       <Script
@@ -25,7 +25,7 @@ function MyApp({ Component, pageProps }) {
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', '${process.env.GA_ID}');`}</Script>
+          gtag('config', '${process.env.REACT_APP_GA_ID}');`}</Script>
       <Component {...pageProps} />
     </>
   );
